df9063455d6429e999759d10dab40c8c87228beb
Juan Rassa <jrassa@titamedia.com>
2022-11-17 20:43:02 -0500

    Implementation of Header.

---
+++ src/components/Header/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { FaShoppingCart } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+import logo from '../../assets/logo.svg';
+import { Container, HeaderContainer, Cart } from './styles';
+
+function Header() {
+  return (
+    <Container>
+      <HeaderContainer>
+        <Link to="/">
+          <img src={logo} alt="Logo" />
+        </Link>
+        <Link to="/cart">
+          <Cart>
+            <div>
+              <span>0</span>
+            </div>
+            <FaShoppingCart size={36} color="#FFF" />
+          </Cart>
+        </Link>
+      </HeaderContainer>
+    </Container>
+  );
+}
+
+export default Header;
